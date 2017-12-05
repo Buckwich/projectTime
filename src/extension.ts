88939c82d7f115707b5512201a2d6cf64078b9e8
simon <s.steinruecken@gmx.net>
2017-12-05 11:19:30 +0100

    manual insert timestamp

--- src/extension.ts
+++ src/extension.ts
@@ -3,49 +3,42 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as http from 'request-light';
-import { StatusBarAlignment } from 'vscode';
+import * as bdb from 'bdb';
 
+
+const url='https://requestb.in/q9cp82q9';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+    console.log(bdb);
+    var env = new bdb.DbEnv();
+    env.openSync({home: '/path/to/your/env'});
+    env.closeSync();
+  
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "projecttime" is nows active!');
+    console.log('Congratulations, your extension "projecttime" is now sactive!');
 
-    http.xhr({url:'https://requestb.in/q9cp82q9',data:'hello',type:'POST'}).then(response=>{
+
+    http.xhr({url, data:'hello',type:'POST'}).then(response=>{
         console.log(response.responseText);
-        });
-    
-    // do more stuff
-    
-    
-
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        });    
+   
+    let disposable = vscode.commands.registerCommand('projectTime.tag', () => {       
+        vscode.window.showInputBox({prompt:"Input the name of the time tag",value:"call"}).then((value:string)=>{
+            http.xhr({url,data:'bye',type:'POST'}).then(response=>{
+                console.log(response.responseText);
+            });
+        });        
     });
 
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-    http.xhr({url:'https://requestb.in/q9cp82q9',data:'bye',type:'POST'}).then(response=>{
+export function deactivate() {   
+    http.xhr({url,data:'bye',type:'POST'}).then(response=>{
         console.log(response.responseText);
-        });
-    console.log('Process Interrupted, exiting');
-
-    for (let index = 0; index < 100; index++) {
-        console.log(index);
-        
-    }
-    
-    console.log('4');
+    });   
 }
